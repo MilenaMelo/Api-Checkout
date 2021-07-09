7c068608d3c19407d3996e4fd533ab93648aa262
milenamelo <milenasmelo@outlook.com>
2021-07-08 22:12:13 -0300

    Products file

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -6,9 +6,31 @@ const { fileReader } = require("../utils/database");
 
 // return products
 async function getProducts(req, res) {
-    const file = await fileReader();
-    console.log(file);
+    let { produtos } = await fileReader();
+    const { categoria, precoInicial, precoFinal } = req.query;
 
+    products = produtos.filter(produto => produto.estoque > 0);
+
+    // case empty array
+    if (!produtos) {
+        return res.status(200).json([]);
+    }
+
+    // valid case array
+    if (categoria) {
+        products = produtos.filter(produto => produto.categoria.toLowerCase() === categoria.toLowerCase());
+    }
+
+    if (precoInicial) {
+        products = produtos.filter(produto => produto.preco >= precoInicial);
+    }
+
+    if (precoFinal) {
+        products = produtos.filter(produto => produto.preco <= precoFinal);
+    }
+
+    // return array according
+    return res.status(200).json(products);
 }
 
 
